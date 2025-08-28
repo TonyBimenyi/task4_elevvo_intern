1b47bb1d12e71de987273ff236f64be897ea60fa
TonyBimenyi <tonyblaisb@gmail.com>
2025-08-28 21:38:26 +0800

    done with header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,18 @@ export default function Header() {
   return (
     <header className="p-4 flex items-center justify-between">
       {/* Left side */}
-      <Link to="/" className="text-xl font-bold">
+      <Link to="/" className="text-3xl font-bold">
         Dashboard Overview
       </Link>
 
       {/* Right side */}
       <nav className="flex items-center space-x-4">
-        <Link to="/add" className="text-sm">
-          Add Job
-        </Link>
+      <Link to="/add">
+        <button className="px-4 py-2 bg-white text-black text-sm font-medium rounded-sm shadow-md hover:bg-white focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-1 transition">
+            + Add Job
+        </button>
+    </Link>
+
       </nav>
     </header>
   );
